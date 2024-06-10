dc044e2f42507c3b61ad015ab97083e278c47cbc
Aveshua Talifa <hello@talifa.pro>
2024-06-10 11:51:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Redi-Project-test/",
+   define: {
+    'process.env': process.env
+  }
 })
